e0b8641de71f366ad9195172b58b3c080f32a903
Flavia <flavia.bulad@gmail.com>
2022-10-21 02:36:15 -0300

    feat: add a warning message at the payment page when the user is not enrolled yet

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -2,8 +2,20 @@ import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 
 import ChoiceOfTicket from '../../../components/ChoiceOfTicket';
+import { useEnrollContext } from '../../../contexts/EnrollContext';
+import Warning from '../../../layouts/Warning';
 
 export default function Payment() {
+  const { enrolled } = useEnrollContext();
+
+  if (!enrolled) {
+    return (
+      <Warning>
+        <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>
+      </Warning>
+    );
+  }
+
   return (
     <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
@@ -13,5 +25,5 @@ export default function Payment() {
 }
 
 const StyledTypography = styled(Typography)`
-  margin-bottom: 37px!important;
+  margin-bottom: 37px !important;
 `;
