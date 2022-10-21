15179b59cae0203b5e56e558b807ae4d1f2f0c22
Lucas <lugablima@gmail.com>
2022-10-21 17:25:23 -0300

    refactor: choice of modality with or without hotel and order summary

--- src/components/ChoiceOfTicket/index.js
+++ src/components/ChoiceOfTicket/index.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import CardsBox from '../../layouts/CardsBox';
+import HotelOrNot from './HotelOrNot';
 import Subtitle from '../../layouts/Subtitle';
 import Ticket from './Ticket'; 
 import useTicket from '../../hooks/api/useTicket';
 import ConfirmButton from '../../layouts/ConfirmButton';
+import OnlineOrderSummary from './OnlineOrderSummary';
 
 export default function ChoiceOfTicket() {
   const [ticketsData, setTicketsData] = useState([]);
@@ -21,48 +23,19 @@ export default function ChoiceOfTicket() {
 
   return (
     <Container>
-      <Subtitle text='Primeiro, escolha sua modalidade de ingresso' />
+      <Subtitle>Primeiro, escolha sua modalidade de ingresso</Subtitle>
       <CardsBox>
         { ticketsData.length ? ticketsData.map(ticket => <Ticket key={ticket.id} name={ticket.name} price={ticket.price} isSelected={ticketSelected.name === ticket.name} setTicketSelected={setTicketSelected}/> ) : '' }
       </CardsBox>
-      <ConfirmBox>
-        { ticketSelected.name === 'Online' ? <>
-          <Subtitle text ='Fechado! O total ficou em '/>
-          <span> R${ticketSelected.price}</span>
-          <Subtitle text = '. Agora é só confirmar'/>
-        </> : ticketSelected.name === 'Presencial' ? <Subtitle text ='Ótimo! Agora escolha sua modalidade de hospedagem '/> : ''}
-      </ConfirmBox>
-      <CardsBox>
-        { ticketSelected.name === 'Presencial' ?
-          <> 
-            <Ticket name='Sem Hotel' price={0} isSelected={hotelOrNot.name === 'Sem Hotel'} setTicketSelected={setHotelOrNot}/>
-            <Ticket name='Com Hotel' price={350} isSelected={hotelOrNot.name === 'Com Hotel'} setTicketSelected={setHotelOrNot}/> 
-          </>  : ''}
-      </CardsBox>
-      <ConfirmBox>
-        { hotelOrNot.name !== undefined && ticketSelected.name === 'Presencial'? <>
-          <Subtitle text ='Fechado! O total ficou em '/>
-          <span> R${ticketSelected.price + hotelOrNot.price}</span>
-          <Subtitle text = '. Agora é só confirmar'/>
-        </> : ''}
-      </ConfirmBox>
-      {( hotelOrNot.name !== undefined && ticketSelected.name === 'Presencial') || ticketSelected.name === 'Online' ? 
-        <ConfirmButton text = 'RESERVAR INGRESSO'/> : ''}
+      { ticketSelected.name === 'Online' ? <OnlineOrderSummary finalPrice={ticketSelected.price} />
+        : ticketSelected.name === 'Presencial' ? <HotelOrNot hotelOrNot={hotelOrNot} setHotelOrNot={setHotelOrNot} /> : ''}
+      { hotelOrNot.name && ticketSelected.name === 'Presencial' ? 
+        <Subtitle>Fechado! O total ficou em <strong>R$ {ticketSelected.price + hotelOrNot.price}</strong>. Agora é só confirmar</Subtitle> : ''}
+      {hotelOrNot.name && ticketSelected.name === 'Presencial' ? <ConfirmButton text = 'RESERVAR INGRESSO'/> : '' }
     </Container>
   );
 }
 
 const Container = styled.div`
-  margin-top: 44px;
-`;
-
-const ConfirmBox = styled.div`
-  display: flex;
-  margin-top: 44px;
-  
-  span {
-    margin-left: 4px;
-    font: 700 20px/23px 'Roboto', sans-serif;
-    color: #8E8E8E;
-  }
+  width: 100%;
 `;
--- src/layouts/ConfirmButton.js
+++ src/layouts/ConfirmButton.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
-import TextButton from './TextButton';
 
 export default function ConfirmButton({ text }) {
   return (
     <Container>
-      <TextButton>
-        {text}
-      </TextButton>
+      {text}
     </Container>
   );
 }
@@ -21,5 +18,13 @@ const Container = styled.button`
     box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
     border-radius: 4px;
     border: none;
+    font: 400 13px/16px 'Roboto', sans-serif;
+    text-align: center;
+    color: #000000;
+    cursor: pointer;
 
+    &:hover {
+    box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12);
+    background-color: #d5d5d5;
+    }
 `;
