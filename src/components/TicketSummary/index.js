0fb86acabb8aed00584c9fc280376a361adaea92
Lucas <lugablima@gmail.com>
2022-10-24 15:02:21 -0300

    feat: add to ticket summary information

--- src/components/TicketSummary/index.js
+++ src/components/TicketSummary/index.js
@@ -2,14 +2,17 @@ import Card from './Card';
 import Subtitle from '../../layouts/Subtitle';
 import SubtitleCard from '../../layouts/SubtitleCard';
 import TextCard from '../../layouts/TextCard';
+import { useTicketContext } from '../../contexts';
 
 export default function TicketSummary() {
+  const { ticketSelected, hotelOrNot } = useTicketContext();
+
   return (
     <>
       <Subtitle>Ingresso escolhido</Subtitle>
       <Card>
-        <TextCard>Presencial + Com Hotel</TextCard>
-        <SubtitleCard>R$ 600</SubtitleCard>
+        {!hotelOrNot.name ? <TextCard>{ ticketSelected.name }</TextCard> : <TextCard>{ ticketSelected.name } + { hotelOrNot.name }</TextCard>}
+        {!hotelOrNot.name ? <SubtitleCard>R$ { ticketSelected.price }</SubtitleCard> : <SubtitleCard>R$ { ticketSelected.price + hotelOrNot.price }</SubtitleCard>}
       </Card>
     </>            
   );
