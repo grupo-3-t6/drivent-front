1dcfd97187cda16a33c703e0032802f205e81797
Lucas <lugablima@gmail.com>
2022-10-25 21:50:25 -0300

    refactor: ChoiceOfTicket component

--- src/components/ChoiceOfTicket/index.js
+++ src/components/ChoiceOfTicket/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import CardsBox from '../../layouts/CardsBox';
@@ -11,22 +11,22 @@ import OnlineOrderSummary from './OnlineOrderSummary';
 import { useTicketContext } from '../../contexts';
 
 export default function ChoiceOfTicket() {
-  const [ticketsData, setTicketsData] = useState([]);
+  // const [ticketsData, setTicketsData] = useState([]);
   const { ticketSelected, setTicketSelected, hotelOrNot } = useTicketContext();
 
   const { tickets } = useTicket();
 
-  useEffect(() => {
-    if(tickets) {
-      setTicketsData(tickets);
-    }
-  }, [tickets]);
+  // useEffect(() => {
+  //   if(tickets) {
+  //     setTicketsData(tickets);
+  //   }
+  // }, [tickets]);
 
   return (
     <Container>
       <Subtitle>Primeiro, escolha sua modalidade de ingresso</Subtitle>
       <CardsBox>
-        { ticketsData.length ? ticketsData.map(ticket => <Ticket key={ticket.id} name={ticket.name} price={ticket.price} isSelected={ticketSelected.name === ticket.name} setTicketSelected={setTicketSelected}/> ) : '' }
+        { tickets && tickets.map(ticket => <Ticket key={ticket.id} id={ticket.id} name={ticket.name} price={ticket.price} isSelected={ticketSelected.name === ticket.name} setTicketSelected={setTicketSelected}/> )}
       </CardsBox>
       { ticketSelected.name === 'Online' ? <OnlineOrderSummary finalPrice={ticketSelected.price} />
         : ticketSelected.name === 'Presencial' ? <HotelOrNot /> : ''}
