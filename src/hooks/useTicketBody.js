b028b21a2553f36697d3696ec19537b74597f89e
Lucas <lugablima@gmail.com>
2022-10-26 17:07:25 -0300

    fix: the stayId field sent must be null when online ticket is selected

--- src/hooks/useTicketBody.js
+++ src/hooks/useTicketBody.js
@@ -2,6 +2,8 @@ import { useTicketContext } from '../contexts/TicketContext';
 
 export default function useTicketBody() {
   const { ticketSelected, hotelOrNot } = useTicketContext();
+  const stayId = ticketSelected.name === 'Online' ? null : hotelOrNot.id;
+  const finalPrice = ticketSelected.name === 'Online' ? ticketSelected.price : (ticketSelected.price + hotelOrNot.price);
 
-  return { ticketId: ticketSelected.id, stayId: hotelOrNot.id, finalPrice: ticketSelected.price + hotelOrNot.price };
+  return { ticketId: ticketSelected.id, stayId, finalPrice };
 }
