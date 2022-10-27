8ce28b557f88603a72793ad609a1101fc2b7d3db
Raphael <raphaelcavalcantesilva1999@gmail.com>
2022-10-27 03:05:37 -0300

    feat: warning for no hotel working

--- src/layouts/Warning.js
+++ src/layouts/Warning.js
@@ -7,12 +7,13 @@ export default function Warning({ children }) {
 const Container = styled.h6`
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
   p {
     height: 46px;
-    width: 388px;
+    width: 465px;
     font: 400 20px 'Roboto', sans-serif;
     text-align: center;
     color: #8e8e8e;
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import Warning from '../../../layouts/Warning';
+import { useTicketContext } from '../../../contexts/TicketContext';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const { ticketInfo } = useTicketContext();
+
+  return (
+    <>
+      { ticketInfo.ticketSelected.name === 'Online' || ticketInfo.hotelOrNot.name === 'Sem Hotel' ?  <Warning> 
+        <p> 
+          Sua modalidade de ingresso não inclui hospedagem
+        </p> 
+        <p>
+          Prossiga para a escolha de atividades
+        </p>
+      </Warning> : ''} 
+    </>
+  );
 }
